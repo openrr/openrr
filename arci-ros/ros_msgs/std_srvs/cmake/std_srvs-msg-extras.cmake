62383f3cc631a94a1487e5bb45afe0a026422904
Takashi Ogura <ogura@smilerobotics.com>
2021-01-04 15:00:39 +0900

    [arci-ros] Remove cmake files

--- arci-ros/ros_msgs/std_srvs/cmake/std_srvs-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(std_srvs_MESSAGE_FILES "")
-set(std_srvs_SERVICE_FILES "srv/Empty.srv;srv/SetBool.srv;srv/Trigger.srv")
--- arci-ros/ros_msgs/std_srvs/cmake/std_srvs-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.installspace.in
-
-_prepend_path("${std_srvs_DIR}/.." "" std_srvs_MSG_INCLUDE_DIRS UNIQUE)
-set(std_srvs_MSG_DEPENDENCIES )
