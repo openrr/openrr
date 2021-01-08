cec82b165d68303db61040bbcdbf9eba33974907
Akihiko Miyadera <miyadera@studio.co.jp>
2021-01-08 20:14:08 +0900

    [arci] Add tests for TrajectoryPoint

--- arci/src/traits/joint_trajectory_client.rs
+++ arci/src/traits/joint_trajectory_client.rs
@@ -37,3 +37,46 @@ pub trait JointTrajectoryClient: Send + Sync {
     ) -> Result<(), Error>;
     async fn send_joint_trajectory(&self, trajectory: Vec<TrajectoryPoint>) -> Result<(), Error>;
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use assert_approx_eq::assert_approx_eq;
+
+    #[test]
+    fn test_trajectory_point() {
+        let mut tp = TrajectoryPoint::new(vec![1.0, -1.0], std::time::Duration::from_secs(1));
+        assert_approx_eq!(tp.positions[0], 1.0);
+        assert_approx_eq!(tp.positions[1], -1.0);
+        assert!(tp.velocities.is_none());
+        assert_eq!(tp.time_from_start, std::time::Duration::from_secs(1));
+        tp.positions = vec![-1.0, 1.0];
+        tp.velocities = Some(vec![1.0, -1.0]);
+        tp.time_from_start = std::time::Duration::from_secs(2);
+        assert_approx_eq!(tp.positions[0], -1.0);
+        assert_approx_eq!(tp.positions[1], 1.0);
+        let vels = tp.velocities.unwrap();
+        assert_approx_eq!(vels[0], 1.0);
+        assert_approx_eq!(vels[1], -1.0);
+        assert_eq!(tp.time_from_start, std::time::Duration::from_secs(2));
+    }
+
+    #[test]
+    fn test_trajectory_point_debug() {
+        let tp = TrajectoryPoint::new(vec![1.0, -1.0], std::time::Duration::from_secs(1));
+        assert_eq!(
+            format!("{:?}", tp),
+            "TrajectoryPoint { positions: [1.0, -1.0], velocities: None, time_from_start: 1s }"
+        );
+    }
+
+    #[test]
+    fn test_trajectory_point_clone() {
+        let tp1 = TrajectoryPoint::new(vec![1.0, -1.0], std::time::Duration::from_secs(1));
+        let tp2 = tp1.clone();
+        assert_approx_eq!(tp2.positions[0], 1.0);
+        assert_approx_eq!(tp2.positions[1], -1.0);
+        assert_approx_eq!(tp1.positions[0], 1.0);
+        assert_approx_eq!(tp1.positions[1], -1.0);
+    }
+}
