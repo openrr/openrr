7f85eafec5695270bbe4690ab5ee7ac9a359e4f8
Taiki Endo <te316e89@gmail.com>
2021-01-15 16:02:37 +0900

    [arci-urdf-viz] Add sleep to test_web.rs to wait for web server to start
    Without it, a connection error occurs in CI:
    
    ```text
    thread 'test_set_get_vel' panicked at 'called `Result::unwrap()` on an `Err` value: http://127.0.0.1:8888/get_joint_positions: Connection Failed: Connect error: Connection refused (os error 111)
    
    Caused by:
        Connection refused (os error 111)', arci-urdf-viz/tests/test_web.rs:14:10
    ```

--- arci-urdf-viz/tests/test_web.rs
+++ arci-urdf-viz/tests/test_web.rs
@@ -10,6 +10,7 @@ fn test_set_get_vel() {
     const PORT: u16 = 8888;
     let web_server = WebServer::new(PORT);
     std::thread::spawn(move || web_server.start());
+    std::thread::sleep(std::time::Duration::from_secs(1)); // Wait for web server to start.
     let c = UrdfVizWebClient::try_new(Url::parse(&format!("http://127.0.0.1:{}", PORT)).unwrap())
         .unwrap();
     let v = c.current_velocity().unwrap();
@@ -29,6 +30,7 @@ fn test_set_get_pose() {
     const PORT: u16 = 8889;
     let web_server = WebServer::new(PORT);
     std::thread::spawn(move || web_server.start());
+    std::thread::sleep(std::time::Duration::from_secs(1)); // Wait for web server to start.
     let c = UrdfVizWebClient::try_new(Url::parse(&format!("http://127.0.0.1:{}", PORT)).unwrap())
         .unwrap();
     let pose = c.current_pose().unwrap();
