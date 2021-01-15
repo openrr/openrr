d2596058804a97e72c4758201301182965fa2414
Akihiko Miyadera <miyadera@studio.co.jp>
2021-01-15 19:20:05 +0900

    [arci-urdf-viz] Add test for create_joint_trajectory_clients

--- arci-urdf-viz/tests/test_client.rs
+++ arci-urdf-viz/tests/test_client.rs
@@ -1,6 +1,9 @@
+mod web_server;
+
 use assert_approx_eq::assert_approx_eq;
 
 use arci_urdf_viz::UrdfVizWebClientConfig;
+use web_server::*;
 
 #[test]
 fn test_urdf_viz_web_client_config_accessor() {
@@ -57,4 +60,26 @@ fn test_urdf_viz_web_client_config_clone() {
     assert_eq!(config2.wrap_with_joint_velocity_limiter, true);
     assert_approx_eq!(config2.joint_velocity_limits[0], 1.0);
     assert_approx_eq!(config2.joint_velocity_limits[1], 2.0);
+}
+
+#[test]
+fn test_create_joint_trajectory_clients() {
+    const DEFAULT_PORT: u16 = 7777;
+    let web_server = WebServer::new(DEFAULT_PORT);
+    std::thread::spawn(move || web_server.start());
+    let configs = vec![
+        UrdfVizWebClientConfig {
+            name: "c1".to_owned(),
+            joint_names: vec!["j1".to_owned(), "j2".to_owned()],
+            wrap_with_joint_velocity_limiter: true,
+            joint_velocity_limits: vec![1.0, 1.0],
+        },
+        UrdfVizWebClientConfig {
+            name: "c2".to_owned(),
+            joint_names: vec!["j1".to_owned(), "j2".to_owned()],
+            wrap_with_joint_velocity_limiter: false,
+            joint_velocity_limits: vec![],
+        },
+    ];
+    let _clients = arci_urdf_viz::create_joint_trajectory_clients(configs);
 }
