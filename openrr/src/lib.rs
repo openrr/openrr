0f0666e7520deb52392ed45604067bf039dabf03
Mitsuharu Kojima <kojima@smilerobotics.com>
2021-01-26 09:20:49 +0900

    [openrr] Export openrr-apps/command as openrr

--- openrr/src/lib.rs
+++ openrr/src/lib.rs
@@ -1,15 +1,23 @@
-pub mod teleop {
-    pub use openrr_teleop::*;
-}
-
-pub mod sleep {
-    pub use openrr_sleep::*;
+pub mod apps {
+    pub use openrr_apps::*;
 }
 
 pub mod client {
     pub use openrr_client::*;
 }
 
+pub mod command {
+    pub use openrr_command::*;
+}
+
 pub mod planner {
     pub use openrr_planner::*;
+}
+
+pub mod sleep {
+    pub use openrr_sleep::*;
+}
+
+pub mod teleop {
+    pub use openrr_teleop::*;
 }
