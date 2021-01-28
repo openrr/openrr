b8448daf1187c44ddd8bacebf97afa08b3bfafcf
Yuki Onishi <yuki.st16@gmail.com>
2021-01-28 15:21:08 +0900

    [openrr-client] Add LocalMove struct

--- openrr-client/src/clients.rs
+++ openrr-client/src/clients.rs
@@ -2,10 +2,12 @@ mod chain_wrapper;
 mod collision_avoid_client;
 mod collision_check_client;
 mod ik_client;
+mod local_move;
 mod print_speaker;
 
 pub use chain_wrapper::*;
 pub use collision_avoid_client::*;
 pub use collision_check_client::*;
 pub use ik_client::*;
+pub use local_move::*;
 pub use print_speaker::*;
