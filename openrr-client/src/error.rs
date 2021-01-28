8a473905e65fe089845e3e65b289a455a3470352
Yuki Onishi <yuki.st16@gmail.com>
2021-01-28 12:19:17 +0900

    [openrr-client] Sort errors in alphabetical order

--- openrr-client/src/error.rs
+++ openrr-client/src/error.rs
@@ -5,22 +5,22 @@ use urdf_rs::UrdfError;
 #[derive(Debug, Error)]
 #[non_exhaustive]
 pub enum Error {
-    #[error("openrr-client: No JointTrajectoryClient={} is found.", .0)]
-    NoJointTrajectoryClient(String),
+    #[error("openrr-client: arci: {:?}", .0)]
+    Arci(#[from] arci::Error),
+    #[error("openrr-client: MismatchedLength {} != {}.", .0, .1)]
+    MismatchedLength(usize, usize),
     #[error("openrr-client: No IkClient={} is found.", .0)]
     NoIkClient(String),
     #[error("openrr-client: No Joint={} is found.", .0)]
     NoJoint(String),
-    #[error("openrr-client: MismatchedLength {} != {}.", .0, .1)]
-    MismatchedLength(usize, usize),
-    #[error("openrr-client: No UrdfPath is specified.")]
-    NoUrdfPath,
     #[error("openrr-client: No JointsPose {} {} is found.", .0, .1)]
     NoJointsPose(String, String),
+    #[error("openrr-client: No JointTrajectoryClient={} is found.", .0)]
+    NoJointTrajectoryClient(String),
     #[error("openrr-client: No ParentDirectory {:?} is found.", .0)]
     NoParentDirectory(PathBuf),
-    #[error("openrr-client: arci: {:?}", .0)]
-    Arci(#[from] arci::Error),
+    #[error("openrr-client: No UrdfPath is specified.")]
+    NoUrdfPath,
     #[error("openrr-client: urdf-rs: {:?}", .0)]
     UrdfRs(#[from] UrdfError),
 }
