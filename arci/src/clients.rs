2a10de06c12f21ca9d5486e876dc4f8a1416ac13
Yuki Onishi <yuki.st16@gmail.com>
2021-03-19 02:34:36 +0900

    [arci] Add dummy_localization

--- arci/src/clients.rs
+++ arci/src/clients.rs
@@ -1,3 +1,4 @@
+mod dummy_localization;
 mod dummy_move_base;
 mod dummy_navigation;
 mod dummy_trajectory_client;
@@ -5,6 +6,7 @@ mod joint_trajectory_clients_container;
 mod joint_velocity_limiter;
 mod partial_joint_trajectory_client;
 
+pub use dummy_localization::*;
 pub use dummy_move_base::*;
 pub use dummy_navigation::*;
 pub use dummy_trajectory_client::*;
---
+++ arci/src/clients/dummy_localization.rs
@@ -0,0 +1,39 @@
+use crate::error::Error;
+use crate::traits::Localization;
+use nalgebra::{Isometry2, Vector2};
+
+#[derive(Debug)]
+pub struct DummyLocalization {
+    pub current_pose: Isometry2<f64>,
+}
+
+impl DummyLocalization {
+    pub fn new() -> Self {
+        Self {
+            current_pose: Isometry2::new(Vector2::new(0.0, 0.0), 0.0),
+        }
+    }
+}
+
+impl Default for DummyLocalization {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Localization for DummyLocalization {
+    fn current_pose(&self, _frame_id: &str) -> Result<Isometry2<f64>, Error> {
+        Ok(self.current_pose)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_get() {
+        let loc = DummyLocalization::new();
+        let current_pose = loc.current_pose("").unwrap();
+        assert_eq!(current_pose, current_pose.inverse()); // only identity mapping satisfies this
+    }
+}
