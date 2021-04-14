4ba4f806475496050a0bf03c041c5146205b3190
Taiki Endo <te316e89@gmail.com>
2021-04-14 10:47:18 +0900

    [openrr-apps] Add flag to show the default setting as TOML

--- openrr-apps/src/bin/robot_command.rs
+++ openrr-apps/src/bin/robot_command.rs
@@ -1,4 +1,3 @@
-use crate::Error as OpenrrAppsError;
 use openrr_apps::{Error, RobotConfig};
 use openrr_command::{RobotCommand, RobotCommandExecutor};
 use std::path::PathBuf;
@@ -11,10 +10,14 @@ use tracing::info;
     about = "An openrr command line tool."
 )]
 struct RobotCommandArgs {
+    /// Path to the setting file.
     #[structopt(short, long, parse(from_os_str))]
     config_path: Option<PathBuf>,
     #[structopt(subcommand)]
-    command: RobotCommand,
+    command: Option<RobotCommand>,
+    /// Prints the default setting as TOML.
+    #[structopt(long)]
+    show_default_config: bool,
 }
 
 #[tokio::main]
@@ -22,13 +25,40 @@ async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt::init();
     let args = RobotCommandArgs::from_args();
     info!("ParsedArgs {:?}", args);
+
+    if args.show_default_config {
+        print!("{}", toml::to_string(&RobotConfig::default()).unwrap());
+        return Ok(());
+    }
+
     if let Some(config_path) = openrr_apps::utils::get_apps_robot_config(args.config_path) {
+        let command = args.command.ok_or(Error::NoCommand)?;
         let robot_config = RobotConfig::try_new(config_path)?;
         openrr_apps::utils::init_with_anonymize(env!("CARGO_BIN_NAME"), &robot_config);
         let client = robot_config.create_robot_client()?;
         let executor = RobotCommandExecutor {};
-        Ok(executor.execute(&client, &args.command).await?)
+        Ok(executor.execute(&client, &command).await?)
     } else {
-        Err(OpenrrAppsError::NoConfigPath)
+        Err(Error::NoConfigPath)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_args() {
+        let bin = env!("CARGO_BIN_NAME");
+        assert!(RobotCommandArgs::from_iter_safe(&[bin]).is_ok());
+        assert!(RobotCommandArgs::from_iter_safe(&[bin, "--show-default-config"]).is_ok());
+        assert!(RobotCommandArgs::from_iter_safe(&[bin, "--config-path", "path", "list"]).is_ok());
+        assert!(RobotCommandArgs::from_iter_safe(&[
+            bin,
+            "--show-default-config",
+            "--config-path",
+            "path"
+        ])
+        .is_ok());
     }
 }
