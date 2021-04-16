788e051d6ee05b9b9c8f93c94edd7ede2315d019
Taiki Endo <te316e89@gmail.com>
2021-04-16 20:55:55 +0900

    [arci] Add implementation note to some trait methods

--- arci/src/traits/speaker.rs
+++ arci/src/traits/speaker.rs
@@ -4,5 +4,18 @@ use auto_impl::auto_impl;
 
 #[auto_impl(Box, Arc)]
 pub trait Speaker: Send + Sync {
+    /// Starts speaking and returns a future that waits until complete the speaking.
+    ///
+    /// # Implementation
+    ///
+    /// The returned future is expected to behave similarly to
+    /// [`std::thread::JoinHandle`] and [`tokio::task::JoinHandle`]:
+    ///
+    /// - Can wait for the operation to complete by `.await`.
+    /// - The operation does not end even if it is dropped.
+    ///
+    /// If the operation may block the current thread for an extended period of
+    /// time, consider [spawning a thread to running blocking
+    /// operations](https://docs.rs/tokio/1/tokio/index.html#cpu-bound-tasks-and-blocking-code).
     fn speak(&self, message: &str) -> Result<WaitFuture, Error>;
 }
