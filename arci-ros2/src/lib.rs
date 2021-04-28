0aa291b7cd4548c5cfe49b404ead9d30775faab2
Takashi Ogura <ogura@smilerobotics.com>
2021-04-28 17:47:00 +0900

    [arci-ros2][CI] Integrate to CI
    * arci-ros2 is built only ros2 feature is enabled.
    * Use openrr-coverage-ci:ros2 tag for coverage

--- arci-ros2/src/lib.rs
+++ arci-ros2/src/lib.rs
@@ -1,7 +1,8 @@
 //! [`arci`] implementation using ROS1.
-
+#[cfg(feature = "ros2")]
 mod cmd_vel_move_base;
-
+#[cfg(feature = "ros2")]
 pub use cmd_vel_move_base::*;
 // re-export
+#[cfg(feature = "ros2")]
 pub use r2r;
