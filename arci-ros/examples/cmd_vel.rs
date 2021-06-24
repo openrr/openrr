abdda0c816ca36c919d01d612621c41f508251e9
chatblanc-ciel <73688885+chatblanc-ciel@users.noreply.github.com>
2021-06-24 20:02:53 +0900

    Create ros1 example

---
+++ arci-ros/examples/cmd_vel.rs
@@ -0,0 +1,27 @@
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    use arci::{BaseVelocity, MoveBase};
+    use arci_ros::RosCmdVelMoveBase;
+
+    // Initialize node
+    arci_ros::init("arci_ros_cmd_vel_example");
+    let c = RosCmdVelMoveBase::new("/cmd_vel");
+    let mut count = 0;
+    let mut vel = BaseVelocity::default();
+    println!("cmd_vel is running!");
+    while count < 100 {
+        vel.x = 0.001 * (count as f64);
+        c.send_velocity(&vel)?;
+        std::thread::sleep(std::time::Duration::from_millis(100));
+        count += 1;
+        println!("{}, {:?}", count, vel);
+    }
+    while count >= 0 {
+        vel.x = 0.001 * (count as f64);
+        c.send_velocity(&vel)?;
+        std::thread::sleep(std::time::Duration::from_millis(100));
+        count -= 1;
+        println!("{}, {:?}", count, vel);
+    }
+    Ok(())
+}
