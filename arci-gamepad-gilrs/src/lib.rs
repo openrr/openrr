73f3f2cd82df06b5a05670bcd93c1f19d2e4a443
Taiki Endo <taiki@smilerobotics.com>
2021-07-13 13:53:17 +0900

    [openrr-teleop] Fix panic in switcher

--- arci-gamepad-gilrs/src/lib.rs
+++ arci-gamepad-gilrs/src/lib.rs
@@ -1,6 +1,9 @@
 //! [`arci::Gamepad`] implementation using [gilrs](https://gitlab.com/gilrs-project/gilrs).
 
 #![warn(rust_2018_idioms)]
+// This lint is unable to correctly determine if an atomic is sufficient to replace the mutex use.
+// https://github.com/rust-lang/rust-clippy/issues/4295
+#![allow(clippy::mutex_atomic)]
 
 use std::{
     collections::HashMap,
--- arci-ros2/src/lib.rs
+++ arci-ros2/src/lib.rs
@@ -9,6 +9,9 @@
     unreachable_pub
 )]
 #![warn(clippy::default_trait_access, clippy::wildcard_imports)]
+// This lint is unable to correctly determine if an atomic is sufficient to replace the mutex use.
+// https://github.com/rust-lang/rust-clippy/issues/4295
+#![allow(clippy::mutex_atomic)]
 
 mod cmd_vel_move_base;
 mod navigation;
