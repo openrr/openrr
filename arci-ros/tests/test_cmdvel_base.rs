e7b5877cd78fd91d3e44e5a2eea4650158cf82e8
chatblanc-ciel <73688885+chatblanc-ciel@users.noreply.github.com>
2021-07-13 11:33:36 +0900

    Add test for `cmd vel base`

--- arci-ros/tests/test_cmdvel_base.rs
+++ arci-ros/tests/test_cmdvel_base.rs
@@ -2,32 +2,53 @@
 use arci::{BaseVelocity, MoveBase};
 
 #[cfg(target_os = "linux")]
+mod msg {
+    rosrust::rosmsg_include!(geometry_msgs / Twist);
+}
+#[cfg(target_os = "linux")]
+use msg::geometry_msgs::Twist;
+#[cfg(target_os = "linux")]
 mod util;
 
 #[cfg(target_os = "linux")]
 #[tokio::test]
 async fn test_cmd_vel() {
+    use std::sync::mpsc;
 
-	let _roscore = util::run_roscore_for(util::Language::None, util::Feature::Publisher);
+    use assert_approx_eq::assert_approx_eq;
 
+    println!("test cmd vel init");
+    let _roscore = util::run_roscore_for(util::Language::None, util::Feature::Publisher);
 
-	let topic_name = String::from("/cmd_vel");
-	arci_ros::init("arci_ros_cmd_vel_test");
+    let topic_name = String::from("test_twist");
+    arci_ros::init("arci_ros_cmd_vel_test");
+    let (tx, rx) = mpsc::channel::<Twist>();
 
-	let sub = rosrust::subscribe(&topic_name, 1, |v: arci_ros::msg::geometry_msgs::Twist| {
-		println!("{:?}", v)
-	})
-	.unwrap();
+    let sub = rosrust::subscribe(&topic_name, 1, move |v: Twist| {
+        println!("{:?}", v);
+        tx.send(v).unwrap();
+    })
+    .unwrap();
 
-	let topic_name = String::from("/cmd_vel");
-	let c = arci_ros::RosCmdVelMoveBase::new(&topic_name);
-	let mut vel = BaseVelocity::default();
-	println!("test cmd_vel is running!");
+    let c = arci_ros::RosCmdVelMoveBase::new(&topic_name);
+    let mut vel = BaseVelocity::default();
+    println!("test cmd_vel is running!");
 
-	for count in 0..100 {
-		vel.x = 0.001 * (count as f64);
-		c.send_velocity(&vel).unwrap();
-		std::thread::sleep(std::time::Duration::from_millis(100));
-		println!("{}, {:?}", count, vel);
-	}
+    for count in 0..100 {
+        vel.x = 0.001 * (count as f64);
+        c.send_velocity(&vel).unwrap();
+        std::thread::sleep(std::time::Duration::from_millis(100));
+        println!("{}, {:?}", count, vel);
+    }
+
+    let mut rv_count = 0_usize;
+    while let Ok(rv) = rx.recv() {
+        if rv_count == 99 {
+            break;
+        } else {
+            assert_approx_eq!(rv.linear.x, 0.001 * (rv_count as f64));
+            println!("{:?}", rv);
+        }
+        rv_count += 1;
+    }
 }
