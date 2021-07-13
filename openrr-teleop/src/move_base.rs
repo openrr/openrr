a2935ab405588cdaded1714d74ed45795f4f2067
chatblanc-ciel <73688885+chatblanc-ciel@users.noreply.github.com>
2021-07-13 17:05:05 +0900

    Add test for `fn new`

--- openrr-teleop/src/move_base.rs
+++ openrr-teleop/src/move_base.rs
@@ -93,8 +93,9 @@ where
 
 #[cfg(test)]
 mod tests {
-    use super::*;
     use arci::DummyMoveBase;
+
+    use super::*;
 
     #[test]
     fn test_move_node_new() {
@@ -102,6 +103,17 @@ mod tests {
         let base = DummyMoveBase::new();
         let node = MoveBaseNode::new(mode.clone(), base);
 
-        
+        assert_eq!(
+            format!("{:?}", node.move_base),
+            format!("{:?}", DummyMoveBase::new())
+        );
+        assert_eq!(node.mode, mode);
+        assert_eq!(node.submode, String::from(""));
+        assert_eq!(
+            format!("{:?}", node.vel),
+            format!("{:?}", BaseVelocity::default())
+        );
+        assert!(!node.is_enabled);
+        assert!(!node.is_turbo);
     }
 }
