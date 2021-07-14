f4213d4aa2528e9ad42be1f970305d469d554a3a
Yuki Onishi <yuki.st16@gmail.com>
2021-07-14 19:47:53 +0900

    [openrr-client] Use SelfCollisionChecker in collision_avoidance_client

--- openrr-client/src/clients/collision_avoidance_client.rs
+++ openrr-client/src/clients/collision_avoidance_client.rs
@@ -1,4 +1,7 @@
+use std::sync::Arc;
+
 use arci::{Error, JointTrajectoryClient, TrajectoryPoint, WaitFuture};
+use openrr_planner::SelfCollisionChecker;
 
 // TODO: speed limit
 fn trajectory_from_positions(
@@ -17,37 +20,34 @@ fn trajectory_from_positions(
     traj
 }
 
-pub struct CollisionAvoidanceClient<'a, T>
+pub struct CollisionAvoidanceClient<T>
 where
     T: JointTrajectoryClient,
 {
     pub client: T,
     /// using_joints and collision_check_robot must share the k::Node instance.
-    pub using_joints: k::Chain<f64>,
-    pub collision_check_robot: &'a k::Chain<f64>,
+    pub collision_checker: Arc<SelfCollisionChecker>,
     pub planner: openrr_planner::JointPathPlanner<f64>,
 }
 
-impl<'a, T> CollisionAvoidanceClient<'a, T>
+impl<T> CollisionAvoidanceClient<T>
 where
     T: JointTrajectoryClient,
 {
     pub fn new(
         client: T,
-        using_joints: k::Chain<f64>,
-        collision_check_robot: &'a k::Chain<f64>,
+        collision_checker: Arc<SelfCollisionChecker>,
         planner: openrr_planner::JointPathPlanner<f64>,
     ) -> Self {
         Self {
             client,
-            using_joints,
-            collision_check_robot,
+            collision_checker,
             planner,
         }
     }
 }
 
-impl<'a, T> JointTrajectoryClient for CollisionAvoidanceClient<'a, T>
+impl<T> JointTrajectoryClient for CollisionAvoidanceClient<T>
 where
     T: JointTrajectoryClient,
 {
@@ -64,12 +64,13 @@ where
         positions: Vec<f64>,
         duration: std::time::Duration,
     ) -> Result<WaitFuture, Error> {
-        self.using_joints
+        self.collision_checker
+            .using_joints
             .set_joint_positions_clamped(&self.current_joint_positions()?);
-        let current = self.using_joints.joint_positions();
+        let current = self.collision_checker.using_joints.joint_positions();
         let traj = self
             .planner
-            .plan_avoid_self_collision(&self.using_joints, &current, &positions)
+            .plan_avoid_self_collision(&self.collision_checker.using_joints, &current, &positions)
             .map_err(|e| Error::Other(e.into()))?;
         self.client
             .send_joint_trajectory(trajectory_from_positions(&traj, duration))
@@ -79,12 +80,17 @@ where
         if trajectory.is_empty() {
             return Ok(WaitFuture::ready());
         }
-        self.using_joints
+        self.collision_checker
+            .using_joints
             .set_joint_positions_clamped(&self.current_joint_positions()?);
-        let current = self.using_joints.joint_positions();
+        let current = self.collision_checker.using_joints.joint_positions();
         let positions = self
             .planner
-            .plan_avoid_self_collision(&self.using_joints, &current, &trajectory[0].positions)
+            .plan_avoid_self_collision(
+                &self.collision_checker.using_joints,
+                &current,
+                &trajectory[0].positions,
+            )
             .map_err(|e| Error::Other(e.into()))?;
         let mut trajs = trajectory_from_positions(&positions, trajectory[0].time_from_start);
 
@@ -92,7 +98,7 @@ where
             let positions = self
                 .planner
                 .plan_avoid_self_collision(
-                    &self.using_joints,
+                    &self.collision_checker.using_joints,
                     &trajectory[i - 1].positions,
                     &trajectory[i].positions,
                 )
