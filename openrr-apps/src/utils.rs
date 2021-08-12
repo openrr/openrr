7562e4c201b51448ad29a61ab19cb30476308544
Taiki Endo <taiki@smilerobotics.com>
2021-08-12 14:48:17 +0900

    [openrr-apps] Clean up config-related code

--- openrr-apps/src/utils.rs
+++ openrr-apps/src/utils.rs
@@ -1,11 +1,15 @@
-use std::{io, path::PathBuf};
+use std::{
+    fs, io,
+    path::{Path, PathBuf},
+};
 
 use rand::prelude::*;
 use tracing::{debug, warn};
 
-use crate::RobotConfig;
+use crate::{RobotConfig, RobotTeleopConfig};
 
 const OPENRR_APPS_CONFIG_ENV_NAME: &str = "OPENRR_APPS_ROBOT_CONFIG_PATH";
+const DEFAULT_JOINT_CLIENT_NAME: &str = "all";
 
 /// Get robot config from input or env OPENRR_APPS_ROBOT_CONFIG_PATH
 pub fn get_apps_robot_config(config: Option<PathBuf>) -> Option<PathBuf> {
@@ -18,6 +22,63 @@ pub fn get_apps_robot_config(config: Option<PathBuf>) -> Option<PathBuf> {
                 PathBuf::from(s)
             })
             .ok()
+    }
+}
+
+pub fn resolve_robot_config(
+    config_path: Option<&Path>,
+    overwrite: Option<&str>,
+) -> anyhow::Result<RobotConfig> {
+    match (config_path, overwrite) {
+        (Some(config_path), Some(overwrite)) => {
+            let s = &fs::read_to_string(&config_path)?;
+            let s = &openrr_config::overwrite_str(s, overwrite)?;
+            Ok(RobotConfig::from_str(s, config_path)?)
+        }
+        (Some(config_path), None) => Ok(RobotConfig::new(config_path)?),
+        (None, overwrite) => {
+            let mut config = RobotConfig::default();
+            config
+                .urdf_viz_clients_configs
+                .push(arci_urdf_viz::UrdfVizWebClientConfig {
+                    name: DEFAULT_JOINT_CLIENT_NAME.into(),
+                    joint_names: None,
+                    wrap_with_joint_position_limiter: false,
+                    wrap_with_joint_velocity_limiter: false,
+                    joint_velocity_limits: None,
+                    joint_position_limits: None,
+                });
+            if let Some(overwrite) = overwrite {
+                let s = &toml::to_string(&config)?;
+                let s = &openrr_config::overwrite_str(s, overwrite)?;
+                config = toml::from_str(s)?;
+            }
+            Ok(config)
+        }
+    }
+}
+
+pub fn resolve_teleop_config(
+    config_path: Option<&Path>,
+    overwrite: Option<&str>,
+) -> anyhow::Result<RobotTeleopConfig> {
+    match (config_path, overwrite) {
+        (Some(teleop_config_path), Some(overwrite)) => {
+            let s = &fs::read_to_string(&teleop_config_path)?;
+            let s = &openrr_config::overwrite_str(s, overwrite)?;
+            Ok(RobotTeleopConfig::from_str(s, teleop_config_path)?)
+        }
+        (Some(teleop_config_path), None) => Ok(RobotTeleopConfig::new(teleop_config_path)?),
+        (None, overwrite) => {
+            let mut config = RobotTeleopConfig::default();
+            config.control_nodes_config.move_base_mode = Some("base".into());
+            if let Some(overwrite) = overwrite {
+                let s = &toml::to_string(&config)?;
+                let s = &openrr_config::overwrite_str(s, overwrite)?;
+                config = toml::from_str(s)?;
+            }
+            Ok(config)
+        }
     }
 }
 
