2b873451aeaa4e5a46c61884ef6d8ffc72f45e8a
chatblanc-ciel <matsumoto9845@spice.ocn.ne.jp>
2021-09-28 19:21:14 +0900

    [arci-ros] add test for `ctrl_client`
    panic pattern

--- arci-ros/tests/test_control_client.rs
+++ arci-ros/tests/test_control_client.rs
@@ -1,5 +1,5 @@
 #![cfg(target_os = "linux")]
-use std::{format, mem::drop, sync::Arc, thread};
+use std::{format, thread};
 
 mod msg {
     rosrust::rosmsg_include!(geometry_msgs / Twist);
@@ -9,7 +9,8 @@ use arci_ros::msg::{
     trajectory_msgs::{JointTrajectory, JointTrajectoryPoint},
 };
 mod util;
-use util::msg_helper::subscribe_helper;
+use arci_ros::subscribe_with_channel;
+use util::run_roscore_and_rosrust_init_once;
 
 /// # Test for generation of control client
 ///
@@ -22,18 +23,18 @@ async fn test_ctrl_client_gen() {
     println!("arci ros ctrl client test");
 
     // setup for ROS
-    let _roscore = util::run_roscore_for(util::Language::None, util::Feature::Publisher);
-    arci_ros::init("arci_ros_ctrl_client_test");
+    let _roscore = run_roscore_and_rosrust_init_once("arci_ros_ctrl_client_test");
 
     // core topic name
-    let topic_name = Arc::new(String::from("test_ctrl_client"));
+    const CONTROLLER_NAME: &str = "test_ctrl_client";
 
     // initialize other actor
-    let topic_cp = topic_name.clone();
     let _handle = thread::spawn(move || {
         // connecting topic (``test_ctrl_client/state<JointTrajectoryControllerState>``)
-        let (_rx, _sub) =
-            subscribe_helper::<JointTrajectoryControllerState>(&format!("{}/state", topic_cp));
+        let (_rx, _sub) = subscribe_with_channel::<JointTrajectoryControllerState>(
+            &format!("{}/state", CONTROLLER_NAME),
+            1,
+        );
 
         let pos = vec![0.0_f64, 0.0];
         let duration = std::time::Duration::from_millis(1000);
@@ -49,9 +50,11 @@ async fn test_ctrl_client_gen() {
             time_from_start: duration.into(),
             ..Default::default()
         };
-        let publisher =
-            rosrust::publish::<JointTrajectoryControllerState>(&format!("{}/state", topic_cp), 1)
-                .unwrap();
+        let publisher = rosrust::publish::<JointTrajectoryControllerState>(
+            &format!("{}/state", CONTROLLER_NAME),
+            1,
+        )
+        .unwrap();
 
         // waiting for subscriber of RosControlClient
         while rosrust::is_ok() && publisher.subscriber_count() == 0 {
@@ -61,8 +64,8 @@ async fn test_ctrl_client_gen() {
         publisher
             .send(JointTrajectoryControllerState {
                 joint_names: vec!["a".to_owned()],
-                actual: actual.clone(),
-                desired: desired.clone(),
+                actual,
+                desired,
                 error: JointTrajectoryPoint::default(),
                 ..Default::default()
             })
@@ -73,7 +76,8 @@ async fn test_ctrl_client_gen() {
         drop(_sub);
 
         // connecting topic (``test_ctrl_client/command<JointTrajectory>``)
-        let (_rx, sub) = subscribe_helper::<JointTrajectory>(&format!("{}/command", topic_cp));
+        let (_rx, sub) =
+            subscribe_with_channel::<JointTrajectory>(&format!("{}/command", CONTROLLER_NAME), 1);
 
         // waiting for publisher of RosControlClient
         while rosrust::is_ok() && sub.publisher_count() == 0 {
@@ -86,5 +90,88 @@ async fn test_ctrl_client_gen() {
     // inner action : waiting for connecting topic
     // - JointTrajectoryControllerState(join controller topic)
     // - JointTrajectory(sending command topic)
-    let _ctrl_client = arci_ros::RosControlClient::new(vec!["a".to_owned()], &topic_name, true);
+    let _ctrl_client = arci_ros::RosControlClient::new(vec!["a".to_owned()], CONTROLLER_NAME, true);
+}
+
+/// # Test for generation of control client
+///
+/// ``Control Client`` needs connecting ``topic_name/state`` , ``topic_name/command``.
+/// This test watch the connecting and if it can not find ``topic name``, generator should ``panic!``.
+/// This test is expected ``panic!``.
+///
+#[tokio::test]
+#[should_panic]
+async fn test_ctrl_client_gen_panic() {
+    println!("arci ros ctrl client test panic");
+
+    // setup for ROS
+    let _roscore = run_roscore_and_rosrust_init_once("test_ctrl_client_gen_panic");
+
+    // core topic name
+    const CONTROLLER_NAME: &str = "test_ctrl_client_panic";
+
+    // initialize other actor
+    let _handle = thread::spawn(move || {
+        // connecting topic (``test_ctrl_client/state<JointTrajectoryControllerState>``)
+        let (_rx, _sub) = subscribe_with_channel::<JointTrajectoryControllerState>(
+            &format!("{}/state", CONTROLLER_NAME),
+            1,
+        );
+
+        let pos = vec![0.0_f64, 0.0];
+        let duration = std::time::Duration::from_millis(1000);
+        let actual = JointTrajectoryPoint {
+            positions: pos.clone(),
+            velocities: vec![0.0; 2],
+            time_from_start: duration.into(),
+            ..Default::default()
+        };
+        let desired = JointTrajectoryPoint {
+            positions: pos,
+            velocities: vec![0.0; 2],
+            time_from_start: duration.into(),
+            ..Default::default()
+        };
+        let publisher = rosrust::publish::<JointTrajectoryControllerState>(
+            &format!("{}/state", CONTROLLER_NAME),
+            1,
+        )
+        .unwrap();
+
+        // waiting for subscriber of RosControlClient
+        while rosrust::is_ok() && publisher.subscriber_count() == 0 {
+            thread::sleep(std::time::Duration::from_secs(1));
+        }
+
+        // This controller should have ``ctrl_client`` 's joint name
+        publisher
+            .send(JointTrajectoryControllerState {
+                joint_names: vec!["b".to_owned()],
+                actual,
+                desired,
+                error: JointTrajectoryPoint::default(),
+                ..Default::default()
+            })
+            .unwrap();
+        if let Ok(rv) = _rx.recv() {
+            println!("mpsc {:?}", rv);
+        }
+        drop(_sub);
+
+        // connecting topic (``test_ctrl_client/command<JointTrajectory>``)
+        let (_rx, sub) =
+            subscribe_with_channel::<JointTrajectory>(&format!("{}/command", CONTROLLER_NAME), 1);
+
+        // waiting for publisher of RosControlClient
+        while rosrust::is_ok() && sub.publisher_count() == 0 {
+            thread::sleep(std::time::Duration::from_secs(1));
+        }
+    });
+
+    // # construct ``arci_ros::RosControlClient``
+    //
+    // inner action : waiting for connecting topic
+    // - JointTrajectoryControllerState(join controller topic)
+    // - JointTrajectory(sending command topic)
+    let _ctrl_client = arci_ros::RosControlClient::new(vec!["a".to_owned()], CONTROLLER_NAME, true);
 }
