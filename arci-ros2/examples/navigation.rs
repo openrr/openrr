2c612e1ace6bb9eb6fbb754f6cbfb9e8dd2fdc21
Taiki Endo <taiki@smilerobotics.com>
2021-11-10 11:33:29 +0900

    [arci-ros2] Update r2r

--- arci-ros2/examples/navigation.rs
+++ arci-ros2/examples/navigation.rs
@@ -1,19 +1,23 @@
 #[cfg(feature = "ros2")]
 #[tokio::main]
-async fn main() -> Result<(), anyhow::Error> {
+async fn main() -> anyhow::Result<()> {
+    use std::time::Duration;
+
     use arci::*;
     use arci_ros2::{r2r, Ros2Navigation};
 
     let ctx = r2r::Context::create().unwrap();
-    let n = Ros2Navigation::new(ctx, "/navigate_to_pose");
-    n.send_goal_pose(
+    let nav = Ros2Navigation::new(ctx, "/navigate_to_pose");
+    nav.send_goal_pose(
         Isometry2::new(Vector2::new(-0.6, 0.2), 1.0),
-        "frame_id",
-        std::time::Duration::from_secs_f64(10.0),
+        "map",
+        Duration::from_secs_f64(10.0),
     )?
     .await?;
     Ok(())
 }
 
 #[cfg(not(feature = "ros2"))]
-fn main() {}
+fn main() {
+    println!("This example requires ros2 feature");
+}
--- arci-ros2/tests/test_cmd_vel.rs
+++ arci-ros2/tests/test_cmd_vel.rs
@@ -1,25 +1,18 @@
 #![cfg(feature = "ros2")]
 
+use arci::{BaseVelocity, MoveBase};
+use arci_ros2::{r2r, Ros2CmdVelMoveBase};
+use assert_approx_eq::assert_approx_eq;
+use futures::stream::StreamExt;
+use r2r::geometry_msgs::msg::Twist;
+
 #[tokio::test]
 async fn test_pub() {
-    use std::sync::mpsc;
-
-    use arci::{BaseVelocity, MoveBase};
-    use arci_ros2::{r2r, Ros2CmdVelMoveBase};
-    use assert_approx_eq::assert_approx_eq;
-    use r2r::geometry_msgs::msg::Twist;
-
     let ctx = r2r::Context::create().unwrap();
     let c = Ros2CmdVelMoveBase::new(ctx.clone(), "/cmd_vel_test");
     let mut node = r2r::Node::create(ctx, "recv", "arci_ros2_test").unwrap();
-    let (tx, rx) = mpsc::channel::<Twist>();
 
-    let _sub = node.subscribe(
-        "/cmd_vel_test",
-        Box::new(move |v: Twist| {
-            tx.send(v).unwrap();
-        }),
-    );
+    let mut sub = node.subscribe::<Twist>("/cmd_vel_test").unwrap();
 
     let mut count = 0;
     let mut vel = BaseVelocity::default();
@@ -27,7 +20,7 @@ async fn test_pub() {
         vel.x = 0.001 * (count as f64);
         c.send_velocity(&vel).unwrap();
         node.spin_once(std::time::Duration::from_millis(10));
-        let v = rx.recv().unwrap();
+        let v = sub.next().await.unwrap();
         assert_approx_eq!(v.linear.x, vel.x);
         assert_approx_eq!(v.linear.y, vel.y);
         assert_approx_eq!(v.linear.z, 0.0);
@@ -44,7 +37,7 @@ async fn test_pub() {
         vel.theta = -0.003 * (count as f64);
         c.send_velocity(&vel).unwrap();
         node.spin_once(std::time::Duration::from_millis(10));
-        let v = rx.recv().unwrap();
+        let v = sub.next().await.unwrap();
         assert_approx_eq!(v.linear.x, vel.x);
         assert_approx_eq!(v.linear.y, vel.y);
         assert_approx_eq!(v.linear.z, 0.0);
