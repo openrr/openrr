a17bcf7d49ce815cb71ea29c5269f5e27e751162
Taiki Endo <taiki@smilerobotics.com>
2022-02-14 15:05:30 +0900

    Use lld in Linux CI

---
+++ ci/ubuntu-setup-lld.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+LLVM_VERSION=13
+
+codename="$(grep '^VERSION_CODENAME=' /etc/os-release | sed 's/^VERSION_CODENAME=//')"
+echo "deb http://apt.llvm.org/${codename}/ llvm-toolchain-${codename}-${LLVM_VERSION} main" |
+    sudo tee "/etc/apt/sources.list.d/llvm-toolchain-${codename}-${LLVM_VERSION}.list"
+curl https://apt.llvm.org/llvm-snapshot.gpg.key |
+    gpg --dearmor |
+    sudo tee /etc/apt/trusted.gpg.d/llvm-snapshot.gpg
+
+sudo apt-get -qq update
+sudo apt-get -o Dpkg::Use-Pty=0 install -y --no-install-recommends \
+    clang-"${LLVM_VERSION}" \
+    lld-"${LLVM_VERSION}"
+
+for tool in /usr/bin/clang*-"${LLVM_VERSION}" /usr/bin/*lld*-"${LLVM_VERSION}"; do
+    link="${tool%"-${LLVM_VERSION}"}"
+    sudo update-alternatives --install "${link}" "${link##*/}" "${tool}" 10
+done
+
+echo "RUSTFLAGS=${RUSTFLAGS} -C linker=clang-${LLVM_VERSION} -C link-arg=-fuse-ld=lld-${LLVM_VERSION}" >> "${GITHUB_ENV}"
