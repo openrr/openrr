d8372ae63348a0742e87d545ef9832d036f051f0
Taiki Endo <taiki@smilerobotics.com>
2022-03-25 21:45:24 +0900

    Update dependencies
    - nalgebra 0.29 -> 0.30
    - ncollide3d 0.32 -> 0.33
    - k 0.27 -> 0.28
    - urdf-viz 0.35 -> 0.36

--- arci-urdf-viz/tests/test_web.rs
+++ arci-urdf-viz/tests/test_web.rs
@@ -21,8 +21,13 @@ impl WebServer {
             if let Some(positions) = handle.take_target_joint_positions() {
                 *handle.current_joint_positions() = positions;
             }
-            if let Some(origin) = handle.take_target_robot_origin() {
-                *handle.current_robot_origin() = origin;
+            if let Some(origin) = handle.pop_target_object_origin() {
+                if origin.id == urdf_viz::ROBOT_OBJECT_ID {
+                    *handle.current_robot_origin() = urdf_viz::RobotOrigin {
+                        position: origin.position,
+                        quaternion: origin.quaternion,
+                    };
+                }
             }
         });
         std::thread::sleep(Duration::from_secs(1)); // Wait for web server to start.
