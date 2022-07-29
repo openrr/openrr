a398df6a7b17e483dd00afd8444cdd02af118982
Taiki Endo <te316e89@gmail.com>
2022-07-29 17:24:27 +0900

    Remove unnecessary allowed lints

--- arci-gamepad-keyboard/src/lib.rs
+++ arci-gamepad-keyboard/src/lib.rs
@@ -1,9 +1,6 @@
 #![doc = include_str!("../README.md")]
 #![cfg(unix)]
 #![warn(missing_docs, rust_2018_idioms)]
-// This lint is unable to correctly determine if an atomic is sufficient to replace the mutex use.
-// https://github.com/rust-lang/rust-clippy/issues/4295
-#![allow(clippy::mutex_atomic)]
 
 use std::{
     collections::HashMap,
