41d81fbd9bf74f0ca46ffa005357b5e8e6971a40
katsu <katsu1112uchi@gmail.com>
2022-08-09 10:18:04 +0900

    [tools] Fix command args in shellscript

--- tools/update-schema.sh
+++ tools/update-schema.sh
@@ -10,5 +10,5 @@ IFS=$'\n\t'
 
 cd "$(cd "$(dirname "${0}")" && pwd)"/..
 
-cargo run -p openrr-apps --bin openrr_apps_config -- schema RobotConfig >openrr-apps/schema/robot_config.json
-cargo run -p openrr-apps --bin openrr_apps_config -- schema RobotTeleopConfig >openrr-apps/schema/robot_teleop_config.json
+cargo run -p openrr-apps --bin openrr_apps_config -- schema robot-config >openrr-apps/schema/robot_config.json
+cargo run -p openrr-apps --bin openrr_apps_config -- schema robot-teleop-config >openrr-apps/schema/robot_teleop_config.json
