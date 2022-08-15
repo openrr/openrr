1f8efae3205615b40801ba577cefd7b9ce9b2963
Taiki Endo <te316e89@gmail.com>
2022-08-15 16:30:00 +0900

    Ignore buggy clippy::derive_partial_eq_without_eq lint

--- openrr-sleep/src/lib.rs
+++ openrr-sleep/src/lib.rs
@@ -1,5 +1,7 @@
 #![doc = include_str!("../README.md")]
 #![warn(rust_2018_idioms)]
+// buggy: https://github.com/rust-lang/rust-clippy/issues?q=is%3Aissue+derive_partial_eq_without_eq
+#![allow(clippy::derive_partial_eq_without_eq)]
 
 mod scoped_sleep;
 
