cc5b16c0bbb90809316ba355713858304046a168
katsu <katsu1112uchi@gmail.com>
2022-08-25 10:32:46 +0900

    [openrr-teleop] Add test robot_command_executor

--- openrr-teleop/src/robot_command_executor.rs
+++ openrr-teleop/src/robot_command_executor.rs
@@ -180,3 +180,118 @@ pub struct RobotCommandConfig {
     pub name: String,
     pub file_path: String,
 }
+
+#[cfg(test)]
+mod test {
+    use std::collections::HashMap;
+
+    use arci::{
+        DummyJointTrajectoryClient, DummyLocalization, DummyMoveBase, DummyNavigation,
+        DummySpeaker, JointTrajectoryClient, Localization, MoveBase, Navigation,
+    };
+    use openrr_client::RobotClient;
+
+    use super::*;
+
+    #[test]
+    fn test_robot_command_executor_inner() {
+        let commands = vec![
+            RobotCommandConfig {
+                name: String::from("name0"),
+                file_path: String::from("path0"),
+            },
+            RobotCommandConfig {
+                name: String::from("name1"),
+                file_path: String::from("path1"),
+            },
+        ];
+        let mut inner = RobotCommandExecutorInner::new(commands);
+
+        assert_eq!(inner.get_command().name, "name0");
+        assert_eq!(inner.get_command().file_path, "path0");
+
+        // Changed submode
+        inner.handle_event(GamepadEvent::ButtonPressed(Button::East));
+        assert_eq!(inner.submode, "name1");
+        assert_eq!(inner.command_index, 1);
+
+        // Case that enable switch is on
+        inner.handle_event(GamepadEvent::ButtonPressed(Button::RightTrigger2));
+        assert!(inner.is_trigger_holding);
+        assert!(!inner.is_sending);
+
+        // Case that enable switch becomes off
+        inner.handle_event(GamepadEvent::ButtonReleased(Button::RightTrigger2));
+        assert!(!inner.is_trigger_holding);
+        assert!(!inner.is_sending);
+
+        // Send command
+        inner.handle_event(GamepadEvent::ButtonPressed(Button::West));
+        assert!(inner.is_sending);
+
+        // Stop send command
+        inner.handle_event(GamepadEvent::ButtonReleased(Button::West));
+        assert!(!inner.is_sending);
+
+        // Disconnected
+        inner.handle_event(GamepadEvent::Disconnected);
+        assert!(!inner.is_trigger_holding);
+        assert!(!inner.is_sending);
+    }
+
+    #[test]
+    fn test_robot_command_executor_new() {
+        let robot_client = Arc::new(
+            RobotClient::new(
+                toml::from_str("urdf_path = \"path\"").unwrap(),
+                {
+                    HashMap::from([(
+                        String::from("arm"),
+                        Arc::new(DummyJointTrajectoryClient::new(vec![String::from(
+                            "test_joint1",
+                        )])) as Arc<dyn JointTrajectoryClient>,
+                    )])
+                },
+                {
+                    HashMap::from([(
+                        String::from("speaker"),
+                        Arc::new(DummySpeaker::new()) as Arc<dyn Speaker>,
+                    )])
+                },
+                Some(Arc::new(DummyLocalization::new()) as Arc<dyn Localization>),
+                Some(Arc::new(DummyMoveBase::new()) as Arc<dyn MoveBase>),
+                Some(Arc::new(DummyNavigation::new()) as Arc<dyn Navigation>),
+            )
+            .unwrap(),
+        );
+
+        let robot_command_executor = RobotCommandExecutor::new(
+            PathBuf::from("path"),
+            vec![RobotCommandConfig {
+                name: String::from("name"),
+                file_path: String::from("file_path"),
+            }],
+            robot_client.clone(),
+            DummySpeaker::new(),
+        )
+        .unwrap();
+
+        assert_eq!(robot_command_executor.base_path, PathBuf::from("path"));
+        assert_eq!(
+            robot_command_executor.inner.lock().commands[0].name,
+            String::from("name")
+        );
+        assert_eq!(
+            robot_command_executor.inner.lock().commands[0].file_path,
+            String::from("file_path")
+        );
+
+        assert!(RobotCommandExecutor::new(
+            PathBuf::from("path"),
+            vec![],
+            robot_client,
+            DummySpeaker::new()
+        )
+        .is_none());
+    }
+}
