0d0beb8aeec8cc58651298f6fa3ab7b40de4b268
katsu <katsu1112uchi@gmail.com>
2022-10-24 19:52:25 +0900

    Update joy gamepad to load toml

--- openrr-apps/src/bin/robot_teleop.rs
+++ openrr-apps/src/bin/robot_teleop.rs
@@ -1,5 +1,5 @@
 #[cfg(feature = "ros")]
-use std::{collections::HashMap, thread};
+use std::thread;
 use std::{fs, path::PathBuf, sync::Arc};
 
 use anyhow::{format_err, Result};
@@ -160,24 +160,10 @@ async fn main() -> Result<()> {
         #[cfg(feature = "ros")]
         GamepadKind::Builtin(BuiltinGamepad::JoyGamepad) => {
             arci_ros::init("joy_gamepad");
-            let mut button_map = HashMap::new();
-            button_map.insert(0, arci::gamepad::Button::South);
-            button_map.insert(1, arci::gamepad::Button::East);
-            button_map.insert(2, arci::gamepad::Button::West);
-            button_map.insert(3, arci::gamepad::Button::North);
-            button_map.insert(4, arci::gamepad::Button::LeftTrigger2);
-            button_map.insert(5, arci::gamepad::Button::RightTrigger2);
-            let mut axis_map = HashMap::new();
-            axis_map.insert(0, arci::gamepad::Axis::LeftStickX);
-            axis_map.insert(1, arci::gamepad::Axis::LeftStickY);
-            axis_map.insert(2, arci::gamepad::Axis::LeftTrigger);
-            axis_map.insert(3, arci::gamepad::Axis::RightStickX);
-            axis_map.insert(4, arci::gamepad::Axis::RightStickY);
-            axis_map.insert(5, arci::gamepad::Axis::RightTrigger);
-            axis_map.insert(6, arci::gamepad::Axis::DPadX);
-            axis_map.insert(7, arci::gamepad::Axis::DPadY);
             switcher
-                .main(arci_ros::JoyGamepad::new("joy", button_map, axis_map))
+                .main(arci_ros::JoyGamepad::new_from_config(
+                    &teleop_config.joy_gamepad_config,
+                ))
                 .await;
         }
         #[cfg(not(feature = "ros"))]
