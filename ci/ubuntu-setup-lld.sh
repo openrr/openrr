19459d090d3e0701edabb75514fce21417a54887
Taiki Endo <te316e89@gmail.com>
2022-12-08 15:34:43 +0900

    Update lld to 15

--- ci/ubuntu-setup-lld.sh
+++ ci/ubuntu-setup-lld.sh
@@ -2,14 +2,16 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-LLVM_VERSION=13
-
 codename="$(grep '^VERSION_CODENAME=' /etc/os-release | sed 's/^VERSION_CODENAME=//')"
+case "${codename}" in
+    bionic) LLVM_VERSION=13 ;;
+    *) LLVM_VERSION=15 ;;
+esac
 echo "deb http://apt.llvm.org/${codename}/ llvm-toolchain-${codename}-${LLVM_VERSION} main" |
-    sudo tee "/etc/apt/sources.list.d/llvm-toolchain-${codename}-${LLVM_VERSION}.list"
+    sudo tee "/etc/apt/sources.list.d/llvm-toolchain-${codename}-${LLVM_VERSION}.list" >/dev/null
 curl https://apt.llvm.org/llvm-snapshot.gpg.key |
     gpg --dearmor |
-    sudo tee /etc/apt/trusted.gpg.d/llvm-snapshot.gpg
+    sudo tee /etc/apt/trusted.gpg.d/llvm-snapshot.gpg >/dev/null
 
 sudo apt-get -qq update
 sudo apt-get -o Dpkg::Use-Pty=0 install -y --no-install-recommends \
@@ -21,4 +23,4 @@ for tool in /usr/bin/clang*-"${LLVM_VERSION}" /usr/bin/*lld*-"${LLVM_VERSION}"; 
     sudo update-alternatives --install "${link}" "${link##*/}" "${tool}" 10
 done
 
-echo "RUSTFLAGS=${RUSTFLAGS} -C linker=clang-${LLVM_VERSION} -C link-arg=-fuse-ld=lld-${LLVM_VERSION}" >> "${GITHUB_ENV}"
+echo "RUSTFLAGS=${RUSTFLAGS} -C linker=clang-${LLVM_VERSION} -C link-arg=-fuse-ld=lld-${LLVM_VERSION}" >>"${GITHUB_ENV}"
