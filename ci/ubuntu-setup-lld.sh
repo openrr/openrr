bb36866992d5866b664b4a1df61007d992b85bb5
Taiki Endo <te316e89@gmail.com>
2023-06-01 17:21:47 +0900

    Update lld to 16

--- ci/ubuntu-setup-lld.sh
+++ ci/ubuntu-setup-lld.sh
@@ -3,24 +3,21 @@ set -euo pipefail
 IFS=$'\n\t'
 
 codename="$(grep '^VERSION_CODENAME=' /etc/os-release | sed 's/^VERSION_CODENAME=//')"
-case "${codename}" in
-    bionic) LLVM_VERSION=13 ;;
-    *) LLVM_VERSION=15 ;;
-esac
-echo "deb http://apt.llvm.org/${codename}/ llvm-toolchain-${codename}-${LLVM_VERSION} main" |
-    sudo tee "/etc/apt/sources.list.d/llvm-toolchain-${codename}-${LLVM_VERSION}.list" >/dev/null
+llvm_version=16
+echo "deb http://apt.llvm.org/${codename}/ llvm-toolchain-${codename}-${llvm_version} main" |
+    sudo tee "/etc/apt/sources.list.d/llvm-toolchain-${codename}-${llvm_version}.list" >/dev/null
 curl https://apt.llvm.org/llvm-snapshot.gpg.key |
     gpg --dearmor |
     sudo tee /etc/apt/trusted.gpg.d/llvm-snapshot.gpg >/dev/null
 
 sudo apt-get -qq update
 sudo apt-get -o Dpkg::Use-Pty=0 install -y --no-install-recommends \
-    clang-"${LLVM_VERSION}" \
-    lld-"${LLVM_VERSION}"
+    clang-"${llvm_version}" \
+    lld-"${llvm_version}"
 
-for tool in /usr/bin/clang*-"${LLVM_VERSION}" /usr/bin/*lld*-"${LLVM_VERSION}"; do
-    link="${tool%"-${LLVM_VERSION}"}"
+for tool in /usr/bin/clang*-"${llvm_version}" /usr/bin/*lld*-"${llvm_version}"; do
+    link="${tool%"-${llvm_version}"}"
     sudo update-alternatives --install "${link}" "${link##*/}" "${tool}" 10
 done
 
-echo "RUSTFLAGS=${RUSTFLAGS} -C linker=clang-${LLVM_VERSION} -C link-arg=-fuse-ld=lld-${LLVM_VERSION}" >>"${GITHUB_ENV}"
+echo "RUSTFLAGS=${RUSTFLAGS} -C linker=clang-${llvm_version} -C link-arg=-fuse-ld=lld-${llvm_version}" >>"${GITHUB_ENV}"
