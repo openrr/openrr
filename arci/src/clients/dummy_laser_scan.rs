b43a11dc1a33ad01466286840116c2e188241288
H1rono <hronok66@gmail.com>
2023-09-08 12:10:31 +0900

    Add `set_scan` in `DummyLaserScan2D`

--- arci/src/clients/dummy_laser_scan.rs
+++ arci/src/clients/dummy_laser_scan.rs
@@ -1,17 +1,25 @@
+use parking_lot::Mutex;
+
 use crate::{Error, LaserScan2D, Scan2D};
 
-#[derive(Debug, Clone, Default)]
-pub struct DummyLaserScan2D;
+#[derive(Debug, Default)]
+pub struct DummyLaserScan2D {
+    scan: Mutex<Scan2D>,
+}
 
 impl DummyLaserScan2D {
     pub fn new() -> Self {
-        Self
+        Self::default()
+    }
+
+    pub fn set_scan(&self, scan: Scan2D) {
+        *self.scan.lock() = scan;
     }
 }
 
 impl LaserScan2D for DummyLaserScan2D {
     fn current_scan(&self) -> Result<Scan2D, Error> {
-        Ok(Scan2D::default())
+        Ok(self.scan.lock().clone())
     }
 }
 
@@ -23,5 +31,18 @@ mod tests {
     fn test_scan() {
         let laser_scan = DummyLaserScan2D::new();
         assert_eq!(laser_scan.current_scan().unwrap(), Scan2D::default());
+        laser_scan.set_scan(Scan2D {
+            angle_min: 1.0,
+            angle_max: 2.0,
+            ..Default::default()
+        });
+        assert_eq!(
+            laser_scan.current_scan().unwrap(),
+            Scan2D {
+                angle_min: 1.0,
+                angle_max: 2.0,
+                ..Default::default()
+            }
+        );
     }
 }
