7e9823d0f5230916a9aaa963d7272f8a7f289a73
Katsu Uchiyama <katsu1112uchi@gmail.com>
2023-09-19 00:51:39 +0900

    Add test for nomotion service

--- arci-ros2/tests/test_ros2_localization_client.rs
+++ arci-ros2/tests/test_ros2_localization_client.rs
@@ -3,6 +3,7 @@
 use arci::Localization;
 use arci_ros2::{r2r, Ros2LocalizationClient};
 use assert_approx_eq::assert_approx_eq;
+use futures::StreamExt;
 use r2r::{
     geometry_msgs::msg::{Point, Pose, PoseWithCovariance, PoseWithCovarianceStamped, Quaternion},
     std_msgs::msg::Header,
@@ -55,4 +56,27 @@ async fn test_localization_client() {
     assert_approx_eq!(current_pose.translation.vector.x, 2.0);
     assert_approx_eq!(current_pose.translation.vector.y, 3.0);
     assert_approx_eq!(current_pose.rotation.angle(), std::f64::consts::FRAC_PI_2);
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_localization_client_nomotion_update() {
+    let ctx = r2r::Context::create().unwrap();
+    let mut node = r2r::Node::create(ctx.clone(), "test_localization_node", "arci_ros2").unwrap();
+
+    let service_server = node
+        .create_service::<r2r::std_srvs::srv::Empty::Service>(NO_MOTION_UPDATE_SERVICE)
+        .unwrap();
+
+    let client = Ros2LocalizationClient::new(ctx, true, NO_MOTION_UPDATE_SERVICE, AMCL_POSE_TOPIC);
+
+    tokio::spawn(async move {
+        loop {
+            match service_server.next().await {
+                Some(req) => todo!(),
+                None => todo!(),
+            }
+        }
+    });
+
+    client.requst_nomotion_update().await;
 }
