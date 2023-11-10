879b0a5db19ba6507015f72a038889852044fa53
Taiki Endo <te316e89@gmail.com>
2023-11-10 21:01:02 +0900

    [arci-urdf-viz] Disable flaky test on macOS

--- arci-urdf-viz/tests/test_client.rs
+++ arci-urdf-viz/tests/test_client.rs
@@ -231,8 +231,8 @@ fn test_send_joint_positions_no_wait() {
 
 #[flaky_test::flaky_test]
 fn test_send_joint_trajectory() {
-    // GitHub Actions' Windows runner is slow.
-    if cfg!(windows) && env::var_os("CI").is_some() {
+    // GitHub Actions' macOS/Windows runner is slow.
+    if (cfg!(windows) || cfg!(target_os = "macos")) && env::var_os("CI").is_some() {
         return;
     }
     test_send_joint_trajectory_inner();
