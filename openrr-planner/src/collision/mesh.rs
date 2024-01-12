2fbcc8bd159b1fcb60e0cc3e659d2d65bce47f0f
Taiki Endo <te316e89@gmail.com>
2024-01-12 21:25:17 +0900

    Update mesh-loader to 0.1

--- openrr-planner/src/collision/mesh.rs
+++ openrr-planner/src/collision/mesh.rs
@@ -21,38 +21,36 @@ where
             Err(err) => Err(Error::MeshError(err.to_owned())),
         }
     } else {
-        // assimp crate only supports utf-8 path
-        match filename.extension().and_then(OsStr::to_str) {
-            Some("stl" | "STL") => load_stl(filename, scale),
-            Some("dae" | "DAE") => load_collada(filename, scale),
-            _ => Err(Error::MeshError(format!("failed to parse {filename:?}"))),
-        }
+        // assimp crate only supports UTF-8 path
+        load_mesh_fallback(filename, scale)
     }
 }
 
 #[cfg(not(feature = "assimp"))]
-pub(crate) fn load_mesh<P, T>(filename: P, scale: &[f64; 3]) -> Result<TriMesh<T>>
+pub(crate) use load_mesh_fallback as load_mesh;
+pub(crate) fn load_mesh_fallback<P, T>(filename: P, scale: &[f64; 3]) -> Result<TriMesh<T>>
 where
     P: AsRef<Path>,
     T: RealField + Copy,
 {
     let filename = filename.as_ref();
-    match filename.extension().and_then(OsStr::to_str) {
-        Some("stl" | "STL") => load_stl(filename, scale),
-        Some("dae" | "DAE") => load_collada(filename, scale),
-        _ => Err(Error::MeshError(format!(
-            "assimp feature is disabled: could not parse {filename:?}"
-        ))),
-    }
-}
+    let scene = match filename.extension().and_then(OsStr::to_str) {
+        Some("stl" | "STL") => mesh_loader::stl::from_slice(&fs::read(filename)?)?,
+        Some("dae" | "DAE") => mesh_loader::collada::from_str(&fs::read_to_string(filename)?)?,
+        _ => {
+            if cfg!(feature = "assimp") {
+                return Err(Error::MeshError(format!(
+                    "assimp feature is enabled but filename is not UTF-8: could not parse {filename:?}"
+                )));
+            } else {
+                return Err(Error::MeshError(format!(
+                    "assimp feature is disabled: could not parse {filename:?}"
+                )));
+            }
+        }
+    };
 
-fn load_stl<P, T>(filename: P, scale: &[f64; 3]) -> Result<TriMesh<T>>
-where
-    P: AsRef<Path>,
-    T: RealField + Copy,
-{
-    let mesh = mesh_loader::stl::from_slice(&fs::read(filename)?)?;
-
+    let mesh = mesh_loader::Mesh::merge(scene.meshes);
     let vertices = mesh
         .vertices
         .iter()
@@ -64,43 +62,11 @@ where
             )
         })
         .collect();
-
     let indices = mesh
         .faces
         .iter()
         .map(|face| na::Point3::new(face[0] as usize, face[1] as usize, face[2] as usize))
         .collect();
-
-    Ok(TriMesh::new(vertices, indices, None))
-}
-
-fn load_collada<P, T>(filename: P, scale: &[f64; 3]) -> Result<TriMesh<T>>
-where
-    P: AsRef<Path>,
-    T: RealField + Copy,
-{
-    let scene = mesh_loader::collada::from_str(&fs::read_to_string(filename)?)?;
-
-    let mut vertices = Vec::new();
-    let mut indices = Vec::new();
-    let mut last_index: usize = 0;
-    for mesh in scene.meshes {
-        vertices.extend(mesh.vertices.iter().map(|v| {
-            na::Point3::<T>::new(
-                na::convert(v[0] as f64 * scale[0]),
-                na::convert(v[1] as f64 * scale[1]),
-                na::convert(v[2] as f64 * scale[2]),
-            )
-        }));
-        indices.extend(mesh.faces.iter().map(|f| {
-            na::Point3::new(
-                f[0] as usize + last_index,
-                f[1] as usize + last_index,
-                f[2] as usize + last_index,
-            )
-        }));
-        last_index = vertices.len();
-    }
 
     Ok(TriMesh::new(vertices, indices, None))
 }
