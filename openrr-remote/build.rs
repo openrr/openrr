3a85d277a6e1032faa837d94d2a8c5b07ce4d03d
Taiki Endo <te316e89@gmail.com>
2024-07-31 19:26:11 +0900

    Resolve unexpected_cfgs warning
    ```
    error: unexpected `cfg` condition name: `local_out_dir`
     --> openrr-remote/src/lib.rs:6:11
      |
    6 |     #[cfg(local_out_dir)]
      |           ^^^^^^^^^^^^^
      |
      = help: expected names are: `clippy`, `debug_assertions`, `doc`, `docsrs`, `doctest`, `feature`, `miri`, `overflow_checks`, `panic`, `proc_macro`, `relocation_model`, `rustfmt`, `sanitize`, `sanitizer_cfi_generalize_pointers`, `sanitizer_cfi_normalize_integers`, `target_abi`, `target_arch`, `target_endian`, `target_env`, `target_family`, `target_feature`, `target_has_atomic`, `target_has_atomic_equal_alignment`, `target_has_atomic_load_store`, `target_os`, `target_pointer_width`, `target_thread_local`, `target_vendor`, `test`, `ub_checks`, `unix`, and `windows`
      = help: consider using a Cargo feature instead
      = help: or consider adding in `Cargo.toml` the `check-cfg` lint config for the lint:
               [lints.rust]
               unexpected_cfgs = { level = "warn", check-cfg = ['cfg(local_out_dir)'] }
      = help: or consider adding `println!("cargo::rustc-check-cfg=cfg(local_out_dir)");` to the top of the `build.rs`
      = note: see <https://doc.rust-lang.org/nightly/rustc/check-cfg/cargo-specifics.html> for more information about checking conditional configuration
      = note: `-D unexpected-cfgs` implied by `-D warnings`
      = help: to override `-D warnings` add `#[allow(unexpected_cfgs)]`
    
    error: unexpected `cfg` condition name: `local_out_dir`
     --> openrr-remote/src/lib.rs:8:15
      |
    8 |     #[cfg(not(local_out_dir))]
      |               ^^^^^^^^^^^^^
      |
      = help: consider using a Cargo feature instead
      = help: or consider adding in `Cargo.toml` the `check-cfg` lint config for the lint:
               [lints.rust]
               unexpected_cfgs = { level = "warn", check-cfg = ['cfg(local_out_dir)'] }
      = help: or consider adding `println!("cargo::rustc-check-cfg=cfg(local_out_dir)");` to the top of the `build.rs`
      = note: see <https://doc.rust-lang.org/nightly/rustc/check-cfg/cargo-specifics.html> for more information about checking conditional configuration
    ```

--- openrr-remote/build.rs
+++ openrr-remote/build.rs
@@ -3,6 +3,7 @@ use std::env;
 fn main() {
     println!("cargo:rerun-if-changed=proto");
     println!("cargo:rerun-if-env-changed=OPENRR_REMOTE_LOCAL_OUT_DIR");
+    println!("cargo:rustc-check-cfg=cfg(local_out_dir)");
 
     let mut config = tonic_build::configure();
     if env::var_os("OPENRR_REMOTE_LOCAL_OUT_DIR").is_some() {
